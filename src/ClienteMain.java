660e7b9fe37411ad7e2fee8bda302f3ff250a3d3
Juan José Osorio <j.osorioc@uniandes.edu.co>
2022-11-01 00:07:59 -0500

    Opciones del pdf
    4, 16 o 32 clients

--- src/ClienteMain.java
+++ src/ClienteMain.java
@@ -6,9 +6,24 @@ public class ClienteMain {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the number of Clients:");
+        System.out.println("Enter (1 for 4), (2 for 16) or (3 for 32) Clients:");
 
-        int numClients = Integer.parseInt(scanner.nextLine());
+        String choice = scanner.nextLine();
+
+        int numClients = 0;
+
+        if (choice.equals("1"))
+            numClients = 4;
+        else if (choice.equals("2"))
+            numClients = 16;
+        else if (choice.equals("3"))
+            numClients = 32;
+        else {
+            System.err.println("Invalid choice");
+            System.exit(-1);
+        }
+
+
         scanner.close();
 
         for (int i = 0; i < numClients; i++) {
